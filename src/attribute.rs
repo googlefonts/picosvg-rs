5a21a7237d03105a44dcbfd1508e49dcf65c6d9d
Rod <rsheeter@google.com>
2022-10-05 14:09:50 -0700

    Attrs (#9)
    * possible attribute model
    
    * rename name -> id for consistency
    
    * add some explanation for the attribute model
    
    * add comment to generated file, fix typos/grammar
    
    Co-authored-by: Chad Brokaw <cbrokaw@gmail.com>

---
+++ src/attribute.rs
@@ -0,0 +1,136 @@
+use std::collections::HashMap;
+use std::string::String;
+
+pub trait ValueDescriptor {
+    fn into_value(self) -> Value;
+    fn try_deref_value(value: &Value) -> Option<&Self>;
+}
+
+macro_rules! value_desc {
+    ($name:ident) => {
+        impl ValueDescriptor for $name {
+            fn into_value(self) -> Value {
+                Value::$name(self)
+            }
+            fn try_deref_value(value: &Value) -> Option<&Self> {
+                match value {
+                    Value::$name(value) => Some(value),
+                    _ => None,
+                }
+            }
+        }
+    };
+}
+
+pub trait AttributeDescriptor {
+    const ID: &'static str;
+    type Value: ValueDescriptor;
+
+    fn id(&self) -> &'static str {
+        Self::ID
+    }
+
+    fn validate(value: &Value) -> bool {
+        Self::Value::try_deref_value(value).is_some()
+    }
+}
+
+macro_rules! attr_desc {
+    ($name:ident, $id:expr, $ty:ty) => {
+        #[derive(Copy, Clone, Debug)]
+        pub struct $name;
+        impl AttributeDescriptor for $name {
+            const ID: &'static str = $id;
+            type Value = $ty;
+        }
+    };
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum Unit {
+    None,
+    Em,
+    Ex,
+    Px,
+    In,
+    Cm,
+    Mm,
+    Pt,
+    Pc,
+    Percent,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Length {
+    pub value: f32,
+    pub unit: Unit,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    Length(Length),
+    Color(Color),
+    String(String),
+}
+
+#[derive(Default, Debug)]
+pub struct AttributeMap {
+    items: HashMap<&'static str, Value>,
+}
+
+impl AttributeMap {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn insert<A: AttributeDescriptor>(
+        &mut self,
+        attr: A,
+        value: impl Into<A::Value>,
+    ) -> Option<Value> {
+        self.items.insert(attr.id(), value.into().into_value())
+    }
+
+    pub fn get<A: AttributeDescriptor>(&self, attr: A) -> Option<&A::Value> {
+        self.items
+            .get(attr.id())
+            .map(|value| A::Value::try_deref_value(value))
+            .flatten()
+    }
+
+    pub fn insert_by_id(&mut self, id: &str, value: Value) -> Result<Option<Value>, InsertError> {
+        let (id, validator) = match ATTRIBUTE_VALIDATORS.binary_search_by(|probe| probe.0.cmp(&id))
+        {
+            Ok(idx) => ATTRIBUTE_VALIDATORS[idx],
+            _ => return Err(InsertError::InvalidAttribute),
+        };
+        if !validator(&value) {
+            return Err(InsertError::InvalidValue);
+        }
+        Ok(self.items.insert(id, value))
+    }
+
+    pub fn get_by_id(&self, id: &str) -> Option<&Value> {
+        self.items.get(id)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&'static str, &Value)> + Clone {
+        self.items.iter().map(|(a, b)| (*a, b))
+    }
+}
+
+#[derive(Debug)]
+pub enum InsertError {
+    InvalidAttribute,
+    InvalidValue,
+}
+
+include!("../generated/attribute.rs");
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use picosvg::attribute::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map = AttributeMap::default();
+
+    map.insert(
+        id::Width,
+        Length {
+            value: 100.,
+            unit: Unit::Percent,
+        },
+    );
+    map.insert(
+        id::StopColor,
+        Color {
+            r: 255,
+            g: 0,
+            b: 255,
+            a: 255,
+        },
+    );
+
+    // expected 'String', got 'Length'
+    // set.insert(id::D, Length { value: 100., unit: Unit::Percent});
+
+    // prints Err(InvalidValue)
+    println!(
+        "{:?}",
+        map.insert_by_id(
+            "d",
+            Value::Length(Length {
+                value: 100.,
+                unit: Unit::Percent
+            })
+        )
+    );
+
+    println!("{:?}", map.get_by_id("width"));
+    println!("{:?}", map.get(id::StopColor));
+    println!("{:?}", map.get_by_id("stop-color"));
+
+    println!(
+        "{:?}",
+        map.insert_by_id("width", Value::String("hello".into()))
+    );
+    println!(
+        "{:?}",
+        map.insert_by_id("lobster", Value::String("hello".into()))
+    );
+
+    for (name, value) in map.iter() {
+        println!("{} => {:?}", name, value);
+    }
 }
